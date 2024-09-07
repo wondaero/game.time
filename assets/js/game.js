05c76f6b21f30a64792e7bab6215b10857bb09ac
wondaero <wondaero@nate.com>
2024-09-07 21:49:48 +0900

    자동 타일 추가

--- assets/js/game.js
+++ assets/js/game.js
@@ -26,8 +26,13 @@ class TimesWar{
                 raf: undefined,
                 duration: 60 * 1000,
                 startTime: undefined
-            }
-
+            },
+            turnCnt: 0,
+            forcedData: {
+                tileIdx: undefined,
+                dummyNum: undefined
+            },
+            winner: undefined
         }
 
         this.ui = {}
@@ -103,7 +108,7 @@ class TimesWar{
                 align-items: center;
                 justify-content: center;
                 flex-direction: column;
-                transition: transform 1s;
+                transition: transform 1s linear(0 0%, 0 2.27%, 0.02 4.53%, 0.04 6.8%, 0.06 9.07%, 0.1 11.33%, 0.14 13.6%, 0.25 18.15%, 0.39 22.7%, 0.56 27.25%, 0.77 31.8%, 1 36.35%, 0.89 40.9%, 0.85 43.18%, 0.81 45.45%, 0.79 47.72%, 0.77 50%, 0.75 52.27%, 0.75 54.55%, 0.75 56.82%, 0.77 59.1%, 0.79 61.38%, 0.81 63.65%, 0.85 65.93%, 0.89 68.2%, 1 72.7%, 0.97 74.98%, 0.95 77.25%, 0.94 79.53%, 0.94 81.8%, 0.94 84.08%, 0.95 86.35%, 0.97 88.63%, 1 90.9%, 0.99 93.18%, 0.98 95.45%, 0.99 97.73%, 1 100%);
                 background: linear-gradient(to bottom, #000, #000070, #000);
                 box-shadow: inset 0 0 10px rgba(255, 255, 255, .5);
                 padding: 5px;
@@ -121,7 +126,11 @@ class TimesWar{
             ${cn('player-interface')}.active{
                 box-shadow: 0 0 100px #fff;
                 background: linear-gradient(135deg, #fff 12px, transparent 0);
-                
+            }
+            ${cn('player-interface')}.active.winner{
+                box-shadow: 0 0 100px gold;
+                transform: scale(1.2);
+                background: gold;
             }
             ${cn('player-interface')} header{
                 display: flex;
@@ -161,7 +170,7 @@ class TimesWar{
                 width: var(--tile-size);
                 height: var(--tile-size);
                 box-sizing: border-box;
-                transition: background .8s, color .5s, transform .5s, opacity .8s;
+                transition: background .5s, color .3s, transform .3s, opacity .5s;
                 cursor: pointer;
                 opacity: 1;
             }
@@ -177,7 +186,7 @@ class TimesWar{
             ${cn('player-interface')} ul li:last-of-type{
                 margin-right: 0;
             }
-            ${cn('board-section')}{
+            ${cn('board-wrapper')}{
                 flex: 1;
                 width: 100%;
                 display: flex;
@@ -185,14 +194,14 @@ class TimesWar{
                 justify-content: center;
                 margin: var(--board-margin);
             }
-            ${cn('board-section')} > div{
+            ${cn('board-wrapper')} > div{
                 display: flex;
                 align-items: center;
                 justify-content: center;
                 flex-wrap: wrap;
                 width: var(--board-size);
             }
-            ${cn('board-section')} .tile{
+            ${cn('board-wrapper')} .tile{
                 width: var(--tile-size);
                 height: var(--tile-size);
                 box-sizing: border-box;
@@ -209,11 +218,65 @@ class TimesWar{
                 font-size: calc(var(--tile-size) * .5);
                 position: relative;
             }
-            ${cn('board-section')} .tile.mark{
-                background: #ff0;
+            ${cn('board-wrapper')} .tile.win-mark{
+                background: gold;
                 color: #000;
                 transform: rotate(5deg);
+                animation: winmarktile .05s infinite alternate;
             }
+            ${cn('toast-popup')}{
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                position: fixed;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                background: rgba(255, 255, 255, .5);
+                z-index: 1;
+                opacity: 1;
+                transition: opacity .5s 1s;
+            }
+            ${cn('toast-popup')}.effect{
+                opacity: 0;
+            }
+            ${cn('toast-popup')} > p{
+                background: #fff;
+                color: #000;
+                padding: 10px;
+                border-radius: 5px;
+                margin: 0;
+                font-size: 24px;
+                transform: scaleX(0);
+                transition: transform 1s linear(0 0%, 0 2.27%, 0.02 4.53%, 0.04 6.8%, 0.06 9.07%, 0.1 11.33%, 0.14 13.6%, 0.25 18.15%, 0.39 22.7%, 0.56 27.25%, 0.77 31.8%, 1 36.35%, 0.89 40.9%, 0.85 43.18%, 0.81 45.45%, 0.79 47.72%, 0.77 50%, 0.75 52.27%, 0.75 54.55%, 0.75 56.82%, 0.77 59.1%, 0.79 61.38%, 0.81 63.65%, 0.85 65.93%, 0.89 68.2%, 1 72.7%, 0.97 74.98%, 0.95 77.25%, 0.94 79.53%, 0.94 81.8%, 0.94 84.08%, 0.95 86.35%, 0.97 88.63%, 1 90.9%, 0.99 93.18%, 0.98 95.45%, 0.99 97.73%, 1 100%);
+            }
+            ${cn('toast-popup')} > p.effect{
+                transform: scaleX(1);
+            }
+
+            .target-circle{
+                position: absolute; width: 30px; height: 30px; margin-top: 10px; margin-left: 10px;
+                background: radial-gradient(rgba(255, 255, 255, .1), transparent, transparent);
+                border: 1px solid rgba(255, 255, 255, .8); transform: scale(0); border-radius: 50%;
+                animation: circle .5s ease-in-out alternate;
+            }
+            .target-bar1{
+                width: 120px; height: 2px; background: #0ff; position: absolute; top: 50%; left: 50%;
+                margin-top: -1px; margin-left: -60px; transform: rotate(-45deg); box-sizing: border-box;
+                border-left: 0; border-right: 0; animation: bar1 .5s ease-in-out alternate;
+            }
+            .target-bar2{
+                width: 100px; height: 2px; position: absolute; top: 50%; left: 50%; box-sizing: border-box;
+                border-left: 10px solid rgba(255, 0, 0, .5); border-right: 10px solid rgba(255, 0, 0, .5);
+                margin-top: -1px; margin-left: -50px; transform: rotate(45deg) scale(1.2); animation: bar2 .5s .2s ease-in-out alternate;
+            }
+
+            @keyframes winmarktile{100%{transform: rotate(0);}}
+
+            @keyframes circle{to{transform: scale(1.5);}}
+            @keyframes bar1{to{transform: rotate(45deg) scaleX(1.5); opacity: 0;}}
+            @keyframes bar2{to{transform: rotate(45deg) scale(0); opacity: 0;}}
         `;
 
         document.head.appendChild(style);
@@ -272,19 +335,18 @@ class TimesWar{
 
         const wrapper = document.createElement('div');
         wrapper.classList.add(t.clssId + 'wrapper');
-        
 
         const innerWrapper = document.createElement('div');
         const innerWrapper2 = document.createElement('div');
         const interface1 = document.createElement('div');
         const interface2 = document.createElement('div');
-        const boardSection = document.createElement('div');
+        const board = document.createElement('div');
 
         t.addClass(innerWrapper, 'inner-wrapper');
         t.addClass(innerWrapper2, 'inner-wrapper2');
         t.addClass(interface1, 'player-interface');
         t.addClass(interface2, 'player-interface');
-        t.addClass(boardSection, 'board-section');
+        t.addClass(board, 'board-wrapper');
 
         setTimeout(() => {
             innerWrapper2.classList.add('effect');
@@ -293,13 +355,13 @@ class TimesWar{
         t.ui.wrapper = wrapper;
         t.ui.player1 = interface1;
         t.ui.player2 = interface2;
-        t.ui.boardSection = boardSection;
+        t.ui.board = board;
 
         document.body.appendChild(wrapper);
         wrapper.appendChild(innerWrapper);
         innerWrapper.appendChild(innerWrapper2);
         innerWrapper2.appendChild(interface2);
-        innerWrapper2.appendChild(boardSection);
+        innerWrapper2.appendChild(board);
         innerWrapper2.appendChild(interface1);
 
         t.setBoard();
@@ -362,7 +424,7 @@ class TimesWar{
         }
     }
 
-    setTileSize(){  //쓴다
+    setTileSize(){
         const ww = window.innerWidth;
         const wh = window.innerHeight;
 
@@ -387,7 +449,6 @@ class TimesWar{
     setBoard(col = 5, row = 5, gap = 5){    //쓴다
         const t = this;
         const board = document.createElement('div');
-        board.id = 'boardWrapper';
         board.dataset.size = `${col}*${row}`;
         
         for(let i = 0; i < row; i++){
@@ -400,37 +461,57 @@ class TimesWar{
 
                 board.appendChild(tile);
 
-                tile.addEventListener('click', () => {
-                    if(t.data.pickedIdx === undefined) return;
+                tile.addEventListener('click', async (e) => {
+                    if(this.data.pickedIdx === undefined) return;
 
-                    t.ui.wrapper.classList.add('freeze');
+                    this.ui.wrapper.classList.add('freeze');
 
-                    const cardNum = t.data['player' + t.data.thisTurn].cards[t.data.pickedIdx];
+                    const tileIdx = this.data.forcedData.tileIdx !== undefined ? this.data.forcedData.tileIdx : +tile.dataset.tileIdx;
+                    const cardNum = this.data.forcedData.dummyNum || this.data['player' + this.data.thisTurn].cards[this.data.pickedIdx];
 
-                    t.calcTileVal(tile.dataset.tileIdx, cardNum);
-                    t.updateBoardUI(tile.dataset.tileIdx);
-                    t.effectTile(tile.dataset.tileIdx);
+                    //board쪽
+                    this.calcTileVal(tileIdx, cardNum);
+                    this.updateBoardUI(tileIdx);
+                    this.effectTile(tileIdx);
 
-                    if(t.chkWinCase(+tile.dataset.tileIdx)){
-                        t.data.wonTiles.forEach((tileIdx) => {
-                            t.ui.boardSection.querySelector('[data-tile-idx="' + tileIdx + '"]').classList.add('mark');
+                    // const point = {
+                    //     top: e.clientY,
+                    //     left: e.clientX
+                    // }
+
+                    // this.setWinTile(point);
+
+                    if(this.chkWinCase(tileIdx)){
+                        this.data.wonTiles.forEach((tileIdx) => {
+                            this.ui.board.querySelector('[data-tile-idx="' + tileIdx + '"]').classList.add('win-mark');
                         })
 
-                        this.ui.wrapper.classList.add('freeze');
                         window.cancelAnimationFrame(this.data.timer.raf);
+                        this.data.winner = this.data.thisTurn;
+
+                        this.setWinnerUI();
 
                         return;
                     };
 
+                    if(this.isLooseGame()){
+                        await this.toastPopup('Time for more fun!');
+                        if(this.forcedTile()) tile.click();
 
-                    t.switchTurn();
-                    t.timerStart();
+                        return;
+                    }
 
+                    await this.switchTurn();
+                    this.timerStart();
+
+                    this.data.forcedData.tileIdx = undefined;
+                    this.data.forcedData.dummyNum = undefined;
+                    this.data.turnCnt++;
                 })
             }
         }
 
-        this.ui.boardSection.appendChild(board);
+        this.ui.board.appendChild(board);
     }
 
     /**
@@ -456,7 +537,7 @@ class TimesWar{
      */
     updateBoardUI(tileIdx){
         const t = this;
-        t.ui.boardSection.querySelectorAll('[data-tile-idx]').forEach((tile, idx) => {
+        t.ui.board.querySelectorAll('[data-tile-idx]').forEach((tile, idx) => {
             tile.textContent = t.data.board[idx];
         })
 
@@ -473,7 +554,7 @@ class TimesWar{
      */
     effectTile(tileIdx, effectType, opt){
         const t = this;
-        const targetTile = t.ui.boardSection.querySelectorAll('[data-tile-idx]')[tileIdx];
+        const targetTile = t.ui.board.querySelectorAll('[data-tile-idx]')[tileIdx];
 
         if(!effectType){
             const effect = document.createElement('div');
@@ -488,7 +569,7 @@ class TimesWar{
                 height: 100%;
                 background: #fff;
                 box-shadow: 0 0 12px #fff;
-                transition: transform .5s, opacity .8s;
+                transition: transform .3s, opacity .5s;
                 transform: scale(3) translateY(-10px) rotate(45deg);
                 color: #000;
                 display: flex;
@@ -520,7 +601,6 @@ class TimesWar{
         this.data.thisTurn = this.data.thisTurn === 1 ? 2 : 1;
         this.ui.wrapper.classList.remove('freeze');
         this.setPlayerActive();
-        return;
     }
 
     setPlayerActive(){
@@ -535,7 +615,6 @@ class TimesWar{
      */
     setNewCard(){
         return new Promise((resolve) => {
-
             const d = this.data;
             const removeCard = this.ui['player' + d.thisTurn].querySelectorAll('[data-card-idx]')[d.pickedIdx];
             removeCard.classList.add('remove');
@@ -629,7 +708,7 @@ class TimesWar{
     }
 
     /**
-     * 
+     * 대각선 체크 후 맞는 값이 있으면 return
      * @param {number} tileIdx 마지막으로 클릭한 타일의 idx
      * @param {string} dir 0130: 1시30분방향(시침), 0430: 4시30분방향(시침)
      * @returns 
@@ -698,8 +777,6 @@ class TimesWar{
 
     timerStart(){
         this.data.timer.startTime = performance.now();
-        console.log(this.data.timer.startTime);
-        console.log(this.data.timer.duration)
         this.timer();
     }
     
@@ -708,7 +785,7 @@ class TimesWar{
         const target = this.ui['player' + this.data.thisTurn].querySelector('[data-id="timer"]');
         const otherTarget = this.ui['player' + (this.data.thisTurn === 1 ? 2 : 1)].querySelector('[data-id="timer"]');
         
-        if(!currTime || currTime - startTime < duration){
+        if((!currTime || currTime - startTime < duration) && !this.data.winner){
             
             otherTarget.style.background = '#888';
             const color = duration - (currTime - startTime) <= 10 * 1000 ? '#f00' : '#fff';
@@ -719,13 +796,158 @@ class TimesWar{
             this.data.timer.raf = window.requestAnimationFrame(this.timer);
         }else{
             window.cancelAnimationFrame(this.data.timer.raf);
-            this.autoAction();
+            if(!this.data.winner) this.autoAction();
         }
     }
 
     autoAction(){
         if(!this.data.pickedIdx) this.data.pickedIdx = this.getRandomNum(0, 2);
         this.updatePlayerCardUI();
-        this.ui.boardSection.querySelectorAll('[data-tile-idx]')[this.getRandomNum(0, 24)].click();
+        this.ui.board.querySelectorAll('[data-tile-idx]')[this.getRandomNum(0, 24)].click();
     }
+
+    isLooseGame(){
+        const haveCards = this.data.board.filter(card => card);
+        if(this.data.turnCnt > 4 && haveCards.length < 2
+        || this.data.turnCnt > 8 && haveCards.length < 3
+        || this.data.turnCnt > 16 && haveCards.length < 4){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    forcedTile(){
+        const targetTiles = this.getClosePosition();
+        const dummyNums = this.getDummyNum();
+
+        if(!targetTiles.length || !dummyNums.length) return false;
+
+        this.data.forcedData.tileIdx = targetTiles[this.getRandomNum(0, targetTiles.length - 1)];
+        this.data.forcedData.dummyNum = dummyNums[this.getRandomNum(0, dummyNums.length - 1)];
+
+        return true;
+    }
+
+    toastPopup(msg, dur){
+        return new Promise((resolve) => {
+            this.ui.wrapper.classList.add('freeze');
+            const toast = document.createElement('div');
+            this.addClass(toast, 'toast-popup');
+            toast.innerHTML = `<p>${msg}</p>`;
+
+            const p = toast.querySelector('p');
+            setTimeout(() => {
+                p.classList.add('effect');
+            })
+            
+            toast.addEventListener('transitionend', (e) => {
+                if(e.propertyName === 'transform'){
+                    toast.classList.add('effect');
+                }else if(e.propertyName === 'opacity'){
+                    toast.remove();
+
+                    resolve();
+                }
+            });
+
+            document.body.appendChild(toast);
+        });
+    }
+
+    getClosePosition(){
+        const rowColArr = this.data.board.map((cards, idx) => `${parseInt(idx / 5)}/${idx % 5}-${cards}`);
+        const filteredArr = rowColArr.filter(cards => cards.split('-')[1]);
+
+        const tmpObj = {};
+        filteredArr.forEach((po) => {
+            const rowCol = po.split('-')[0].split('/');
+            const row = +rowCol[0];
+            const col = +rowCol[1];
+
+            const adjVal = [-1, 0, 1];
+
+            for(let i = 0; i < adjVal.length; i++){
+                for(let j = 0; j < adjVal.length; j++){
+                    if(i === 1 && j === 1) continue;
+
+                    let adjRow = row + adjVal[i];
+                    let adjCol = col + adjVal[j];
+
+                    if(0 <= row + adjRow && adjRow <= 4
+                    && 0 <= col + adjCol && adjCol <= 4){
+                        if(tmpObj && tmpObj[adjRow + '-' + adjCol]){
+                            tmpObj[adjRow + '-' + adjCol]++;
+                        }else{
+                            tmpObj[adjRow + '-' + adjCol] = 1;
+                        }
+                    }
+                }
+            }
+        });
+
+        let mxVal = 0;
+        let rtnVal = [];
+
+        for(let key in tmpObj){
+            if(mxVal < tmpObj[key]){
+                mxVal = tmpObj[key];
+                rtnVal = [];
+                rtnVal.push(key);
+            }else if(mxVal === tmpObj[key]){
+                rtnVal.push(key);
+            }
+        }
+
+        rtnVal = rtnVal.map(el => (+el.split('-')[0] * 5) + +el.split('-')[1]);
+
+        return rtnVal;
+    }
+
+    getDummyNum(){
+        const currNums = this.data.board.filter(tile => tile);
+
+        let sampleNums = [1,2,3,4,5,6,7,8,9,10,11,12];
+        const adJVal = [-1, 0, 1];
+
+        currNums.forEach((num) => {
+            adJVal.forEach((adj) => {
+                sampleNums = sampleNums.filter(sn => sn !== num + adj);
+            });
+        })
+
+        return sampleNums;
+    }
+
+    setWinnerUI(){
+        const winner = this.data.winner;
+        this.ui['player' + winner].classList.add('winner');
+    }
+
+
+    /**
+     * 클릭? 터치? 효과
+     */
+    // setWinTile(point){
+    //     const div = document.createElement('div');
+    //     const div2 = document.createElement('div');
+    //     const div3 = document.createElement('div');
+
+    //     div.classList.add('target-circle');
+    //     div2.classList.add('target-bar1');
+    //     div3.classList.add('target-bar2');
+
+    //     console.log(point);
+
+    //     div.style.cssText = `top: 50%; left: 50%;`;
+
+    //     div.appendChild(div2);
+    //     div.appendChild(div3);
+
+    //     div.addEventListener('animationend', function(){
+    //         this.remove();
+    //     })
+
+    //     document.body.appendChild(div);
+    // }
 }
